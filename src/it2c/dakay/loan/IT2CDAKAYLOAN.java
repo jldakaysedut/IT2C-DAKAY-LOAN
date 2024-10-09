1b3bd6ff36499dcb52a2e8294ed88bb5627817fa
Lucelmae <lemoranlucelmae@gmail.com>
2024-10-09 23:35:04 +0800

    updated sql loan

--- src/it2c/dakay/loan/IT2CDAKAYLOAN.java
+++ src/it2c/dakay/loan/IT2CDAKAYLOAN.java
@@ -8,24 +8,31 @@ public class IT2CDAKAYLOAN {
 
 
     
-      public void addStudents(){
+      public void addApplicant(){
         Scanner sc = new Scanner(System.in);
         config conf = new config();
-        System.out.print("First Name: ");
-        String fname = sc.next();
-         System.out.print("Last Name: ");
-        String lname = sc.next();
+        System.out.print("Full Name (family name/given name/middle initial): ");
+        String fname = sc.nextLine();
+         System.out.print("Date of birth (month/day/year): ");
+        String birth = sc.nextLine();
         System.out.print("Age: ");
-        String age = sc.next();
+        String age = sc.nextLine();
+        System.out.print("Contact: ");
+        String contact = sc.nextLine();
+         System.out.print("Address (purok/barangay/city/province): ");
+        String address = sc.nextLine();
+         System.out.print("Email Address: ");
+        String email = sc.nextLine();
         System.out.print("Status (Employed,Unemployed): ");
-        String status = sc.next();
+        String status = sc.nextLine();
         System.out.print("Monthly income: ");
-        String monthlyincome = sc.next();
+        String monthlyincome = sc.nextLine();
 
-        String sql = "INSERT INTO tbl_applicants (A_fname, A_lname, A_age, A_status, A_monthlyincome) VALUES (?, ?, ?, ?, ?)";
+        
+        String sql = "INSERT INTO tbl_applicants (A_fname, A_birth, A_age, A_contact, A_address, A_email, A_status, A_monthlyincome) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
 
-        conf.addRecord(sql, fname, lname, age, status, monthlyincome);
+        conf.addRecord(sql, fname, birth, age, contact, address, email, status, monthlyincome);
 
 
     }
@@ -35,22 +42,34 @@ public class IT2CDAKAYLOAN {
         String response;
        
         do{
-            System.out.println("\n1. Add Person");
-            System.out.println("2. View Persons");
-            System.out.println("3. Delete Person");
-            System.out.println("4. Update Person");
+            System.out.println("\n1. Add Applicant");
+            System.out.println("2. View Applicant");
+            System.out.println("3. Update Applicant");
+            System.out.println("4. Delete Applicant");
             System.out.println("5. Back to Main Menu");
             System.out.print("\nChoose an option: ");
            
             System.out.print("\n Enter Action: ");
             int action = sc.nextInt();
-            
-            switch(action){
+            IT2CDAKAYLOAN demo = new IT2CDAKAYLOAN();
+         
+          switch(action){
                 
-                case 1:
-                IT2CDAKAYLOAN demo = new IT2CDAKAYLOAN();
-                demo.addStudents();
+                case 1:    
+                demo.addApplicant();            
+                break;
                 
+                case 2:
+                demo.viewApplicant();
+                    break;
+                case 3:
+                demo.viewApplicant();
+                demo.updateApplicant();
+                break;
+                case 4:
+                demo.viewApplicant();
+                demo.deleteApplicant(); 
+                demo.viewApplicant();
                 break;
            
         
@@ -67,5 +86,75 @@ public class IT2CDAKAYLOAN {
     
     
 
+private void viewApplicant() {
+        String aqry = "SELECT * FROM tbl_applicants";
+        String[] hrds = {"ID", "FULL Name", "DATE OF BIRTH", "AGE", "CONTACT NUMBER", "ADDRESS", "EMAIL", "STATUS", "MONTHLY INCOME"};
+        String[] clmns = {"A_id", "A_fname", "A_birth", "A_age", "A_contact", "A_address", "A_email", "A_status", "A_monthlyincome"};
+        config conf = new config();
+        conf.viewRecords(aqry, hrds, clmns);
+    }
+
+
+
+   private void updateApplicant(){
+   Scanner sc = new Scanner(System.in);
+   
+   System.out.print("Enter Applicant ID: ");
+   int id = sc.nextInt();
+            sc.nextLine(); 
+            
+        System.out.print("Updated Full Name (family name/given name/middle initial): ");
+        String ufname = sc.nextLine();
+         System.out.print("Updated Date of birth (month/day/year): ");
+        String ubirth = sc.nextLine();
+        System.out.print("Updated Age: ");
+        String uage = sc.nextLine();
+        System.out.print("Updated Contact: ");
+        String ucontact = sc.nextLine();
+         System.out.print("Updated Address (purok/barangay/city/province): ");
+        String uaddress = sc.nextLine();
+         System.out.print("Updated Email Address: ");
+        String uemail = sc.nextLine();
+        System.out.print("Updated Status (Employed, Unemployed): ");
+        String ustatus = sc.nextLine();
+        System.out.print("Updated Monthly income: ");
+        String umonthlyincome = sc.nextLine();  
+      
+      String qry = "UPDATE tbl_applicants SET A_fname = ?, A_birth = ?, A_age = ?, A_contact = ?, A_address = ?, A_email = ?, A_status = ?, A_monthlyincome = ? WHERE A_id = ? ";
     
-}
+   config conf = new config();
+   conf.updateRecord(qry, ufname, ubirth, uage, ucontact, uaddress, uemail, ustatus, umonthlyincome, id);
+   
+   
+   
+   }
+   
+   
+   
+   
+  private void deleteApplicant(){
+
+       Scanner sc = new Scanner(System.in);
+   System.out.print("Enter Applicant ID to delete: ");
+   int id = sc.nextInt(); 
+   
+   String sqlDelete = "DELETE FROM tbl_applicants WHERE A_id = ?";
+   config conf = new config();
+   conf.deleteRecord(sqlDelete, id);
+    }
+  
+  
+  
+  
+  
+  
+  
+} 
+   
+   
+   
+   
+   
+ 
+
+    
